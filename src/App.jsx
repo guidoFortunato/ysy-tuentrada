5add9760c2834eed32916eb404b9fa05a01e6fd3
guido fortunato <guido_fortunato@hotmail.com.ar>
2021-12-21 12:30:17 -0300

    merge con test

--- src/App.jsx
+++ src/App.jsx
@@ -58,9 +58,9 @@ function App() {
             <Footer />           
           </Route> 
 
-          <Route path='/:id' exact >
+          {/* <Route path='/:id' exact >
             <DetalleEvento />            
-          </Route>       
+          </Route>        */}
 
 
           {/* <Route path='*' component={Error404} /> */}
