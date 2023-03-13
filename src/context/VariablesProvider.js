b7d835bb51bb2b428be662d3faa8782941f57e31
guido fortunato <guido_fortunato@hotmail.com.ar>
2023-03-13 17:00:33 -0300

    cambios

--- src/context/VariablesProvider.js
+++ src/context/VariablesProvider.js
@@ -48,9 +48,9 @@ const VariablesProvider = (props) => {
     tituloHistoriaAbajo: "",
     imagenHistoria: historia,
     descripcion1:
-      "Alejo Nahuel Acosta, mejor conocido en el mundo del espectáculo como Ysy A, es un cantante y compositor argentino que nació el 12 de julio del año 1998. Este intérprete se convirtió rápidamente en uno de los cantantes de rap y hip hop con mayor influencia en Argentina. El artista inició su carrera musical, desde muy joven, como un rapero freestyle en las calles de la ciudad de Buenos Aires",
+      "Alejo Nahuel Acosta Migliarini (Buenos Aires, Argentina; 12 de julio de 1998), conocido artísticamente como YSY A, es un rapero, productor, freestyler y diseñador argentino, principalmente del género del trap. Alejo Acosta se hizo conocido en 2015 cuando formó la competencia de rap 'El Quinto Escalón' con apenas 17 años. En 2016 la competición se popularizó de manera desenfrenada, y se volvería la más importante de Sudamérica, además de ser cuna de muchos artistas emergentes de la escena del trap argentino.",
     descripcion2:
-      "",
+      "En 2018, formó parte del trío musical 'Modo Diablo', junto a los raperos Duki y Neo Pistea y el 11 de noviembre de ese mismo año lanzó su álbum de estudio debut, Antezana 247, que tuvo una gran recepción pór parte del público y críticos. El 11 de noviembre de 2019 publicó su segundo álbum de estudio, Hecho a Mano. En 2020 lanzó una serie de sencillos bajo el nombre de #YSYA2020, de los cuales 'Silbando' y 'Un Flow de Infarto' sumaron millones de visitas en YouTube, y sacó su primer EP, Mordiendo El Bozal. El 11 de noviembre de 2021 publicó su tercer álbum de estudio, Trap de Verdad, bajo su propio sello discográfico, Sponsor Dio$. Un año después, en la misma fecha, sacó su cuarto álbum de estudio, YSYSMO, que se convirtió en su disco más exitoso a nivel comercial, teniendo el debut más grande en las listas de éxitos a pesar de no contar con sencillos ni colaboraciones.",
   };
 
   const [variables, setVariables] = React.useState(variablesGlobales);
